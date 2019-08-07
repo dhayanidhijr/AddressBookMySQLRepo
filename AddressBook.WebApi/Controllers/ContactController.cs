385d9f8d5f9a566a6709558b58d4ce2d8f6074d2
Dhaya JR <dhayanidhi.jr@gmail.com>
2019-08-06 23:36:12 -0400

    AddressBook Repository updated with MySql Connection

---
+++ AddressBook.WebApi/Controllers/ContactController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BusinessInterface = AddressBookBusinessLib.Interface;
+using BusinessModel = AddressBookBusinessLib.Model;
+
+namespace AddressBook.WebApi.Controllers
+{
+    /// <summary>
+    /// Contact Controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        BusinessInterface.IContactRepository contactRepository;
+
+        /// <summary>
+        /// Contact Controller
+        /// </summary>
+        /// <param name="contactRepository">Contact Repository Interface</param>
+        public ContactController(BusinessInterface.IContactRepository contactRepository)
+        {
+            this.contactRepository = contactRepository;
+        }
+        
+        /// <summary>
+        /// Get All Active Contact Records
+        /// </summary>
+        /// <returns>Array of Contact Objects</returns>
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<BusinessModel.Contact>> Get()
+        {
+            return contactRepository.ReadAll().ToList();
+        }
+
+        /// <summary>
+        /// Get Contact Record for provided record id
+        /// </summary>
+        /// <param name="id">Record of the contact record which has to be reterived</param>
+        /// <returns>Contact record object for the provided id</returns>
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<BusinessModel.Contact> Get(int id)
+        {
+            return contactRepository.Read(id);
+        }
+
+
+        /// <summary>
+        /// Use to save any new Contact entry
+        /// </summary>
+        /// <param name="contact">Contact object provided to be created as new entry</param>
+        /// <returns>Status OK when successfully created and Not Found when the creation failed</returns>
+        // POST api/values
+        [HttpPost]
+        public ActionResult Post([FromBody] BusinessModel.Contact contact)
+        {
+            if (contactRepository.Create(contact))
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Use to update any existing Contact record
+        /// </summary>
+        /// <param name="id">Record of existing contact record</param>
+        /// <param name="contact">Contact changes encapsulated as contact object which has to be updated</param>
+        /// <returns>Status OK when the update is successful and Not Found when the operation is failed</returns>
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] BusinessModel.Contact contact)
+        {
+            contact.Id = id;
+            if (contactRepository.Update(contact))
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Use to delete any existing Contact Record
+        /// </summary>
+        /// <param name="id">Record Id of existing contact record which is required to be deleted</param>
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            contactRepository.Delete(id);
+        }
+    }
+}
---
+++ AddressBook.WebApi/Startup.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.Logging;
+
+namespace AddressBook.WebApi
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //  services.Configure<AddressBookDataLib.Interface.IDatabaseSetting>(Configuration.GetSection("Database"));
+
+            AddressBookDataLib.Interface.IDatabaseSetting databaseSetting = new AddressBookDataLib.Settings.Database()
+            {
+                ConnectionString = Configuration.GetSection("Database").GetSection("ConnectionString").Value
+            };
+
+            AddressBookDataLib.Interface.IDBContext<AddressBookDataLib.Context.AddressBook> dbContext = new AddressBookDataLib.Context.AddressBook(databaseSetting);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton(databaseSetting);
+
+            services.AddSingleton(dbContext);
+
+            services.AddTransient<AddressBookDataLib.Interface.IAddressRepository, AddressBookDataLib.Repository.AddressRepository>();
+
+            services.AddTransient<AddressBookBusinessLib.Interface.IAddressRepository, AddressBookBusinessLib.Repository.AddressRepository>();
+
+            services.AddTransient<AddressBookDataLib.Interface.IContactRepository, AddressBookDataLib.Repository.ContactRepository>();
+
+            services.AddTransient<AddressBookBusinessLib.Interface.IContactRepository, AddressBookBusinessLib.Repository.ContactRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1.0.0", new Info { Title = "AddressBook", Version = "v1.0.0" });
+                c.IncludeXmlComments(GetWebApiXMLDocumentPath());
+            });
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        /// <param name="logger"></param>
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        {
+
+            logger.AddLog4Net();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("../swagger/v1.0.0/swagger.json", "AddressBook");
+            });
+        }
+
+        private string GetWebApiXMLDocumentPath()
+        {
+            return string.Format("{0}/AddressBookWebApi.xml", System.AppDomain.CurrentDomain.BaseDirectory);
+        }
+    }
+}
